d84e668e87037d5d04263ed4cfbfd91d9f01dd3d
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:49:44 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,39 +1,36 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import m from './';
 
 function Foo(x) {
 	this.x = 1;
 }
 
-function ObjectConstructor() {
-}
+function ObjectConstructor() {}
 
 ObjectConstructor.prototype.constructor = Object;
 
-test(function (t) {
-	t.assert(fn({}));
-	t.assert(fn({foo: true}));
-	t.assert(fn({constructor: Foo}));
-	t.assert(fn({valueOf: 0}));
-	t.assert(fn(Object.create(null)));
-	t.assert(fn(new Object()));
-	t.assert(!fn(arguments));
-	t.assert(!fn(['foo', 'bar']));
-	t.assert(!fn(new Foo(1)));
-	t.assert(!fn(Math));
-	t.assert(!fn(Error));
-	t.assert(!fn(function () {}));
-	t.assert(!fn(/./));
-	t.assert(!fn(null));
-	t.assert(!fn(undefined));
-	t.assert(!fn(NaN));
-	t.assert(!fn(''));
-	t.assert(!fn(0));
-	t.assert(!fn(false));
-	t.assert(!fn(new ObjectConstructor()));
-	var foo = new Foo();
+test(t => {
+	t.true(m({}));
+	t.true(m({foo: true}));
+	t.true(m({constructor: Foo}));
+	t.true(m({valueOf: 0}));
+	t.true(m(Object.create(null)));
+	t.true(m(new Object()));
+	t.false(m(arguments));
+	t.false(m(['foo', 'bar']));
+	t.false(m(new Foo(1)));
+	t.false(m(Math));
+	t.false(m(Error));
+	t.false(m(() => {}));
+	t.false(m(/./));
+	t.false(m(null));
+	t.false(m(undefined));
+	t.false(m(NaN));
+	t.false(m(''));
+	t.false(m(0));
+	t.false(m(false));
+	t.false(m(new ObjectConstructor()));
+	const foo = new Foo();
 	foo.constructor = Object;
-	t.assert(!fn(foo));
-	t.end();
+	t.false(m(foo));
 });
